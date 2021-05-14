f6a2e7162a948d00ccac5279e06dbbb14ba449d9
sujan pal <sujanpal79@gmail.com>
2021-05-15 00:38:08 +0530

    fix: Modify App component

---
+++ src/_info/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKvzREXtrq78aG1EycgUHBXM6VU4moAUQ",
+    authDomain: "clone-9089.firebaseapp.com",
+    projectId: "clone-9089",
+    storageBucket: "clone-9089.appspot.com",
+    messagingSenderId: "34708730747",
+    appId: "1:34708730747:web:a36b9689e5d072259103de",
+    measurementId: "G-RM2BDGM364"
+  };
