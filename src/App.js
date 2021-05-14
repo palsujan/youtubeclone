92c11bd8665a077671991f42187631c7ddccebc6
sujan pal <sujanpal79@gmail.com>
2021-05-15 01:25:58 +0530

    feat: Install Dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './header/Header';
 
 function App() {
   return (
     //BEM class naming conversion
     <div className="App">
       <h1>Hello Programmer</h1>
+      {/* Header */}
+      <Header/>
+      {/* Sidebar */}
+      {/* RecommendVideos */}
     </div>
   );
 }
---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classNames from "classnames"
+import Styles from './Header.module.scss';
+const Header = () => {
+    return (
+        <div className={classNames(Styles.main)}>
+            
+        </div>
+    )
+}
+
+export default Header
